23e65fae396a2938886b83ce5660028f79242a35
RajaReddy <rajareddy511@gmail.com>
2015-12-04 11:15:51 +0530

    Initial commit

---
+++ app/src/main/java/com/polamr/contentproviderssample/TodoTable.java
@@ -0,0 +1,39 @@
+package com.polamr.contentproviderssample;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Rajareddy on 04/12/15.
+ */
+public class TodoTable {
+    // Database table
+    public static final String TABLE_TODO = "todo";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_CATEGORY = "category";
+    public static final String COLUMN_SUMMARY = "summary";
+    public static final String COLUMN_DESCRIPTION = "description";
+
+    // Database creation SQL statement
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_TODO
+            + "("
+            + COLUMN_ID + " integer primary key autoincrement, "
+            + COLUMN_CATEGORY + " text not null, "
+            + COLUMN_SUMMARY + " text not null,"
+            + COLUMN_DESCRIPTION
+            + " text not null"
+            + ");";
+
+    public static void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    public static void onUpgrade(SQLiteDatabase database, int oldVersion,
+                                 int newVersion) {
+        LogUtils.show("Upgrading database from version "
+                + oldVersion + " to " + newVersion
+                + ", which will destroy all old data");
+        database.execSQL("DROP TABLE IF EXISTS " + TABLE_TODO);
+        onCreate(database);
+    }
+}
